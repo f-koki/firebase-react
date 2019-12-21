9b98ec1e77bdd11b6edfbf1710d8e785872398f4
f-koki <fukushimakoki0108@gmail.com>
2019-12-22 00:27:32 +0900

    try: Firebaseを実装しようとした

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import firebase from "firebase";
+import SampleData from "./SampleData";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAOvxYUIFXeRNdjJt6hL7673z3wXZDgfa4",
+  authDomain: "moon-ogiri.firebaseapp.com",
+  databaseURL: "https://moon-ogiri.firebaseio.com",
+  projectId: "moon-ogiri",
+  storageBucket: "moon-ogiri.appspot.com",
+  messagingSenderId: "956945824736",
+  appId: "1:956945824736:web:ad64ff6a2494167f2d8985",
+  measurementId: "G-Q8Q298B81Z"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>fire</h1>
+        <h2>sample data</h2>
+        <SampleData />
+      </div>
+    );
+  }
+}
+
+export default App;
